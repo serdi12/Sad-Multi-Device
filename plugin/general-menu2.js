d37b94342f5201e31663235bcc4d71b88f558772
serdi12 <109755111+serdi12@users.noreply.github.com>
2022-07-24 01:37:45 +0700

    Update and rename plugins/general-menu2.js to plugin/general-menu2.js

---
+++ plugin/general-menu2.js
@@ -0,0 +1,15 @@
+import moment from 'moment-timezone'
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
+let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+conn.sendHydrated(m.chat, '*『 𝚒𝚢𝚊𝚗 』*', `ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ 𝚁𝙲𝚃𝙸𝙱𝙾𝚃_ᴏғᴄ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ 𝚒𝚢𝚊𝚗 ᴏғғɪᴄɪᴀʟ,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://saweria.co/IyanMd', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
+[`ᴍᴇɴᴜ`, '.m'],
+[`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
+], m, {asLocation: true})
+}
+handler.help = ['.menu']
+handler.tags = ['general']
+handler.customPrefix = /^(menu|.menu)$/i
+handler.command = new RegExp
+
+export default handler
